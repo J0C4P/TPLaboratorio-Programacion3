6c6187f3dea279c748e464c4f56b73f7fe533096
J0C4P <j0c4pa1bar@gmail.com>
2026-09-12 23:31:29 -0300

    Renderizar productos mediante clase productos y listados de productos

---
+++ js/producto.js
@@ -0,0 +1,14 @@
+class Producto{
+  constructor(id, nombre, descripcion, categoria, precio, stock, imagen, creador, localidad, favorito){
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.categoria = categoria;
+    this.precio = precio;
+    this.stock = stock;
+    this.imagen = imagen;
+    this.creador = creador;
+    this.localidad = localidad;
+    this.favorito = favorito;
+  }
+}
